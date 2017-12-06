dca1b5b30de4d005fe78d165306256a5714cdf18
Rihards Murnieks <rihardsmurnieks@Rihardss-iMac.local>
2017-12-06 18:12:26 +0200

    just some OOP practice in python

--- createjson.py
+++ createjson.py
@@ -1,16 +1,34 @@
 
+
 import json
+
+class JSONcreation: 
+
+	def __init__(self, name, title, text):
+		self.name = name
+		self.title = title
+		self.text = text
+
+
+	def jsonnotation(self):
+		json_string0 = '{"author":"%s", "title":"%s","body":"%s"}' % (name, title, text)
+		json_string = json_string0
+		return json_string 
+
 
 print 'Enter your name: '
 name=raw_input()
 
 print 'Enter the title of your message: '
 title=raw_input()
+
 print 'Enter the message text: '
 text=raw_input()
 
-json_string0 = '{"author":"%s", "title":"%s","body":"%s"}' % (name, title, text)
-json_string = json_string0
+JSONnotation = JSONcreation(name, title, text)
+json_string = JSONnotation.jsonnotation()
+
+
 #parsed_json=json.loads(json_string)
 #print 'Your name is ' + parsed_json['author']
 #print 'Title of the message is ' + parsed_json['title']
